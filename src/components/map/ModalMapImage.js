c9ab6e2c8fe9eb5df4bc0ddc2eccd685310234fb
Karas Patrik <patrik.karas@extern.a1.at>
2023-12-09 22:09:23 +0100

    updated images

--- src/components/map/ModalMapImage.js
+++ src/components/map/ModalMapImage.js
@@ -2,14 +2,14 @@ import React, { useEffect, useRef } from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import styles from '../../styles/Modal.module.css';
-import { Button } from '@mui/material';
+import { Button, useMediaQuery } from '@mui/material';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: '50%',
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
@@ -17,18 +17,34 @@ const style = {
 };
 
 const ModalMapImage = (props) => {
+  const query = useMediaQuery('(max-width: 600px)');
   const { open, handleClose, resImage } = props;
   const imgRef = useRef(null);
 
   const downloadImage = () => {
-    const link = document.createElement('a');
-    link.href = resImage;
-    link.download = `map-${new Date().getTime()}.png`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    handleClose();
+    const img = new Image();
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = 700; // Set the desired width
+      canvas.height = 700; // Set the desired height
+  
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL('image/png');
+      link.download = `map-${new Date().getTime()}.png`;
+  
+      document.body.appendChild(link); // Append the link to the document
+      link.click(); // Simulate a click on the link to trigger the download
+      document.body.removeChild(link); // Remove the link from the document
+      handleClose();
+    };
+  
+    img.src = resImage;
   };
+  
+  
 
   useEffect(() => {
     if (imgRef.current && resImage && resImage.length > 0) {
@@ -45,7 +61,17 @@ const ModalMapImage = (props) => {
         aria-describedby='modal-modal-description'
         className={styles.modalWrapper}
       >
-        <Box sx={style}>
+        <Box sx={{
+           position: 'absolute',
+           top: '50%',
+           left: '50%',
+           transform: 'translate(-50%, -50%)',
+           width: query ? '90%' : '50%',
+           bgcolor: 'background.paper',
+           border: '2px solid #000',
+           boxShadow: 24,
+           p: 4,
+        }}>
           <div style={{ minHeight: '100hv', width: '100%' }}>
             <h3>Analyzed Area</h3>
             <hr />
