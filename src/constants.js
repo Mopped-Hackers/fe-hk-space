77c49f1af1671dc1799b8f82919e26935e6db344
Karas Patrik <patrik.karas@extern.a1.at>
2023-12-10 02:22:15 +0100

    changed lat and lon

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 
 export const ACCESS_TOKEN = "pk.eyJ1IjoiYW5kcmVqdnlzbnkiLCJhIjoiY2s3cDNvbzJuMDVjMDNlcnJ2NmJramQ0biJ9.3Xs0PsiiF7U5sfibbpA_PA";
 export const MAP_STYLE = "mapbox://styles/mapbox/satellite-streets-v12"
-export const LONG = 19.35843
-export const LAT = 43.3567
+export const LONG = 19.404020
+export const LAT = 43.362642
 export const BACKEND_URL = "https://mopped-spaceport.azurewebsites.net"
 export const DEFAULT_ZOOM = 15
 export const DEMO = 'demo'
